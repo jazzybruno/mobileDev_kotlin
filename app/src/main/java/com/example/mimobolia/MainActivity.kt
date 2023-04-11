f3fc066adb8fb1db43d01c3490139fde79c125d3
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-04-11 15:19:05 +0200

    Addin views on the xml file and adding events

--- app/src/main/java/com/example/mimobolia/MainActivity.kt
+++ app/src/main/java/com/example/mimobolia/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.mimobolia
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnshowToast = findViewById<Button>(R.id.btnShowToast);
+        btnshowToast.setOnClickListener {
+            Log.i("Activity" , "The button has been clicked!!");
+            Toast.makeText(this , "Button was clicked!!" , Toast.LENGTH_SHORT).show()
+        }
     }
 }
