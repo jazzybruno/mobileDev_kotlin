23fa1767c1f25010554aba71609aeef47b28e399
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-04-13 00:28:12 +0200

    Data sharing in mobile develpment kotlin

--- app/src/main/java/com/example/mimobolia/MainActivity.kt
+++ app/src/main/java/com/example/mimobolia/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mimobolia
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -9,12 +10,14 @@ import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // the views declaration
         val btnshowToast = findViewById<Button>(R.id.btnShowToast);
-        val userinputmessage = findViewById<EditText>(R.id.editMessage)
+        val usernameInput = findViewById<EditText>(R.id.editUsername)
+        val passwordInput = findViewById<EditText>(R.id.editPassword);
         val btnNavigateToActivity = findViewById<Button>(R.id.messageToNextActivity)
 
         btnshowToast.setOnClickListener {
@@ -23,11 +26,22 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnNavigateToActivity.setOnClickListener {
-            var message : String = userinputmessage.text.toString();
-            Toast.makeText(this , message , Toast.LENGTH_LONG).show();
+            var message : String ? = null;
+            var username : String = usernameInput.text.toString();
+            var password : String = passwordInput.text.toString();
 
-            val intent : Intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent);
+            if(username == "jazzy" && password == "Bruno@1980"){
+                message = "Successfully logged in";
+                Toast.makeText(this , message , Toast.LENGTH_LONG).show();
+
+                val intent : Intent = Intent(this, SecondActivity::class.java)
+                intent.putExtra("username" , username);
+                startActivity(intent);
+            }else{
+                message = "Failed to login"
+                Toast.makeText(this , message , Toast.LENGTH_LONG).show();
+            }
+
         }
     }
 }
--- app/src/main/java/com/example/mimobolia/SecondActivity.kt
+++ app/src/main/java/com/example/mimobolia/SecondActivity.kt
@@ -2,10 +2,20 @@ package com.example.mimobolia
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        // the views of this
+        val textView : TextView = findViewById(R.id.textView)
+
+        var bundle : Bundle ? = intent.extras;
+        var msg = bundle!!.getString("username");
+
+        // set the value of the text to the username
+        textView.text = msg;
     }
 }
